4b034975ed788705b37c10ec570b5198bac3e918
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-03-10 12:42:13 +0100

    Updates main to be a little faster
    Apparently in julia things in functions run faster, or something

--- main.jl
+++ main.jl
@@ -6,9 +6,10 @@ include("five_point_stensil.jl")
 include("nine_point_stensil.jl")
 using SparseArrays
 using Arpack
+using DelimitedFiles
 
 
-if PROGRAM_FILE == basename(@__FILE__)
+function main()
     ##########################################
     ### Ser parameters, LEVEL and GRID_RES ###
     ##########################################
@@ -16,6 +17,9 @@ if PROGRAM_FILE == basename(@__FILE__)
     PLOTTING = true
     LEVEL = 3
     GRID_RES = 1
+    NEV = 10
+    WIRE = false
+    SAVE = true
 
     if length(ARGS) >= 1
         LEVEL = parse(Int, ARGS[1])
@@ -29,6 +33,7 @@ if PROGRAM_FILE == basename(@__FILE__)
         println("No argument provided, using default GRID_RES=1")
     end
 
+
     ###########################################################
     ### Make lattice, laplacian matrix and solce EV-problem ###
     ###########################################################
@@ -41,16 +46,30 @@ if PROGRAM_FILE == basename(@__FILE__)
     #lap_mat = nine_point_laplacian(N, lattice, points_inside)
 
     println("Solving EV-problem")
-    eigvals, eigvecs = eigs(lap_mat, nev=10, which=:SM)
+    eigvals, eigvecs = eigs(lap_mat, nev=NEV, which=:SM)
     sorted_indices = sortperm(eigvals)
 
     grid = zeros(size(lattice, 1), size(lattice, 1), 10)
-    surf_grid = fill(NaN, (size(lattice, 1), size(lattice, 1), 10))
+    if WIRE
+        wireframe_grid = fill(NaN, (size(lattice, 1), size(lattice, 1), 10))
+    end
 
     TOTAL_MEMORY_MB = 8030668
     mem_used = sizeof(lap_mat)*1e-6
     println("Bytesize of laplacian matrix (MB): ", mem_used)
     println("Percentage of total: ", mem_used*100/TOTAL_MEMORY_MB)
+
+    if SAVE
+        # Save eigenvalues and vectors to file
+        filename = string("eigenvals_and_vecs_level", LEVEL, "gridres", GRID_RES, ".txt")
+        open(filename, "w") do eigen
+            writedlm(eigen, LEVEL)
+            writedlm(eigen, GRID_RES)
+            writedlm(eigen, N)
+            writedlm(eigen, eigvals)
+            writedlm(eigen, eigvecs)
+        end
+    end
 
     ####################
     ### Plot results ###
@@ -58,51 +77,65 @@ if PROGRAM_FILE == basename(@__FILE__)
 
     if PLOTTING
         println("Plotting")
-"""
-        plt.pcolormesh(lap_mat)
-        plt.title(string("level: ", LEVEL, ", grid_res: ", GRID_RES))
-        plt.savefig("laplacian_matrix.pdf")
-        plt.show()
-"""
+        if (LEVEL < 3) && (GRID_RES < 2)
+            plt.pcolormesh(lap_mat)
+            plt.title(string("level: ", LEVEL, ", grid_res: ", GRID_RES))
+            plt.savefig("laplacian_matrix.pdf")
+            #plt.show()
+            plt.cla()
+            plt.clf()
+        end
 
-        for i in 1:10
+        for i in 1:NEV
             idx = sorted_indices[i]
-            for (j, p) in enumerate(points_inside)
+            for (j, p) in Base.Iterators.enumerate(points_inside)
                 grid[p[1], p[2], i] = eigvecs[:, i][j]
-                #surf_grid[p[1], p[2], i] = eigvecs[:, i][j]
+                #wireframe_grid[p[1], p[2], i] = eigvecs[:, i][j]
             end
 
-"""
-            for (j, p) in enumerate(points_border)
-                surf_grid[p[1], p[2], i] = 0
+            if WIRE
+                for (j, p) in enumerate(points_border)
+                    surf_grid[p[1], p[2], i] = 0
+                end
             end
-"""
-            plt.imshow(transpose(grid[:, :, i]), origin="upper")
+
+            if LEVEL > 3
+                plt.imshow(transpose(grid[:, :, i][1:10:end]), origin="upper")
+            else
+                plt.imshow(transpose(grid[:, :, i]), origin="upper")
+            end
             plt.plot(first.(frac) .- 1 , last.(frac) .- 1)
             plt.title(string("eigenmode #", i, ", fractal LEVEL: ", LEVEL, ", GRID_RES: ", GRID_RES))
             plt.savefig(string("eigenmode_2d", i, ".png"))
-            plt.show()
-            #plt.close()
+            #plt.show()
+            plt.cla()
+            plt.clf()
 
             xy = collect(1: size(lattice, 1))
             plt.title(string("eigenmode #", i, ", fractal LEVEL: ", LEVEL, ", GRID_RES: ", GRID_RES))
-            if length(ARGS) >= 3
-                if ARGS[3] == "wire"
-                    plt.plot_wireframe(xy, xy, transpose(surf_grid[:, :, i] ./ 10), color="gray")
-                    plt.plot(first.(frac) , last.(frac), color="red")
-                    plt.savefig(string("eigenmode_3d_wireframe", i, ".png"))
+            if WIRE
+                plt.plot_wireframe(xy, xy, transpose(surf_grid[:, :, i] ./ 10), color="gray")
+                plt.plot(first.(frac) , last.(frac), color="red")
+                plt.savefig(string("eigenmode_3d_wireframe", i, ".png"))
+            else
+                if LEVEL > 3
+                    plt.surf(grid[:, :, i][1:10:end], cmap=plt.cm.coolwarm, alpha=1)
                 else
                     plt.surf(grid[:, :, i], cmap=plt.cm.coolwarm, alpha=1)
                 end
-            else
-                plt.surf(grid[:, :, i], cmap=plt.cm.coolwarm, alpha=1)
             end
             plt.plot(first.(frac) , last.(frac), color="red")
             plt.savefig(string("eigenmode_3d", i, ".png"))
-            plt.show()
-            #plt.close()
+            #plt.show()
+            plt.cla()
+            plt.clf()
         end
     end
+end
+
+
+if PROGRAM_FILE == basename(@__FILE__)
+    main()
 end
 
 
